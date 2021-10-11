6771e280c29ef48ba06202931569200a30a057b0
MUHAMMAD ALI <malirashid1994@gmail.com>
2021-10-11 17:50:01 +0500

    Updated streamlit app context

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,30 +16,36 @@ def main():
     st.title("Auto911 Platform")
     """Let AI help you!"""
 
-    ex_questions = ["""
-    sequence: "My father is having chest pain"
-labels: ["Medical"]
-###
-sequence: "The neighbor is punching someone in his front yard"
-labels: ["Police"]
-###
-sequence: "I just witnessed an accident"
-labels: ["Medical"]
-###
-sequence: "My mother just collapsed"
-labels: ["Medical"]
-###
-sequence: "I see smoke coming out of my neighbors house"
-labels: ["Fire"]
-###
-sequence: "Flames are rising out of the building?"
-labels: ["Fire"]
-###
-sequence: "I can see a man stealing the toyota corola car"
-labels: ["Police"]
-###
-""",
-    ]
+#     ex_questions = ["""
+#     sequence: "My father is having chest pain"
+# labels: ["Medical"]
+# ###
+# sequence: "The neighbor is punching someone in his front yard"
+# labels: ["Police"]
+# ###
+# sequence: "I just witnessed an accident"
+# labels: ["Medical"]
+# ###
+# sequence: "My mother just collapsed"
+# labels: ["Medical"]
+# ###
+# sequence: "I see smoke coming out of my neighbors house"
+# labels: ["Fire"]
+# ###
+# sequence: "Flames are rising out of the building?"
+# labels: ["Fire"]
+# ###
+# sequence: "I can see a man stealing the toyota corola car"
+# labels: ["Police"]
+# ###
+# """,
+#     ]
+
+
+import requests
+
+
+
 
     inp = st.text_area("Please enter your problem here",ex_questions,max_chars=2000, height=150)
 
@@ -49,15 +55,43 @@ labels: ["Police"]
 
         if submit_button:
 
-            payload = {
-                "context": inp+ex_questions[0],
-                "token_max_length": 50,
-                "temperature": 0.35,
-                "top_p": 0.9,
-            }
+            # payload = {
+            #     "context": inp+ex_questions[0],
+            #     "token_max_length": 50,
+            #     "temperature": 0.35,
+            #     "top_p": 0.9,
+            # }
 
-            query = requests.post("http://api.vicgalle.net:5000/classify/", params=payload)
-            response = query.json()
+            payload = { 
+                "sequence" : "My father is having chest pain", 
+                "labels" : "Medical",
+                
+                "sequence" : "The neighbor is punching someone in his front yard", 
+                "labels" : "Police",
+                
+                "sequence" : "I just witnessed an accident", 
+                "labels" : "Medical",
+                
+                "sequence" : "My father just collapsed", 
+                "labels" : "Medical",
+                
+                "sequence" : "I see smoke coming out of my neighbors house", 
+                "labels" : "Fire",
+                
+                "sequence" : "Flames are rising out of the building?", 
+                "labels" : "Fire",
+
+                "sequence" : "I can see a man stealing the toyota corola car",
+                "labels" : "Police",
+                
+                }
+
+
+            response = requests.post("http://api.vicgalle.net:5000/classify", params=payload).json()
+            # print(response)
+
+            # query = requests.post("http://api.vicgalle.net:5000/classify/", params=payload)
+            # response = query.json()
 
             st.markdown(response["labels"]) # ["prompt"] + response["text"]
 
