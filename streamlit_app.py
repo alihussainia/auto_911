a88b4537e337f7ce4e31d6a1d2f07729fa03fbd8
MUHAMMAD ALI <muhammad.ali@dpschool.io>
2022-11-21 02:25:17 +0500

    Testing initial index of response json

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ elif submit_button and inp!="":
  
   response = requests.post(API_URL, headers=headers, json=payload).json()
 
-  st.markdown(response['generated_text']) 
+  st.markdown(response[0]) 
 
 
 st.text("App developed with ❤️ by @alihussainia")
